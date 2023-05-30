500a7c6c3d739ba7abf41f0418defe269982d3d4
Larissa Maehara <larissalali.m@gmail.com>
2023-05-29 21:51:49 -0300

    exercicio02

---
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-01-primeiros-passos-no-javascript/exercicio02/exercicio02.js
@@ -0,0 +1,10 @@
+let num1 = 30;
+let num2 = 20;
+
+if (num1 > num2) {
+    console.log(num1 + " é maior que " + num2);
+} else if (num1 === num2) {
+    console.log("Os números são iguais.");
+} else {
+    console.log(num2 + " é maior que " + num1);
+};
