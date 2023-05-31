b991f6cfcd3f83e21eeddf6d03eac89d2cedeafc
Larissa Maehara <larissalali.m@gmail.com>
2023-05-31 13:51:20 -0300

    atualizando exercicio array e loop 1

--- 01-fundamentos/secao-03-introducao-a-javascript/dia-02-array-e-estrutura-de-repeticao/exercicios/array-e-loop1.js
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-02-array-e-estrutura-de-repeticao/exercicios/array-e-loop1.js
@@ -1,5 +1,6 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+//1-Percorra o array imprimindo todos os valores contidos nele com a função console.log()
 for (index = 0; index < numbers.length; index += 1) {
-    // console.log(groceryList[index]);
     console.log(`${index} - ${numbers[index]}`);
 };
