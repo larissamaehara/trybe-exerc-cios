34f7092d678b899d9ee81ef6e0bb767c05cf5cac
Larissa Maehara <larissalali.m@gmail.com>
2023-05-31 15:29:03 -0300

    array e loop - exercicio 4

--- 01-fundamentos/secao-03-introducao-a-javascript/dia-02-array-e-estrutura-de-repeticao/exercicios/array-e-loop4.js
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-02-array-e-estrutura-de-repeticao/exercicios/array-e-loop4.js
@@ -3,3 +3,17 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 //4-Com base no código que acabou de gerar, referente ao cálculo da média aritmética, faça com que: 
 // - caso o valor final seja maior que 20, imprima a mensagem “O valor da média aritmética é maior que 20”; 
 // - caso não seja maior que 20, imprima a mensagem “O valor da média aritmética é menor ou igual a 20”.
+
+let soma = 0;
+let media;
+
+for (let index = 0; index < numbers.length; index += 1) {
+    soma += numbers[index];
+    media = soma / numbers.length;
+};
+
+if (media > 20) {
+    console.log('O valor da média aritmética é maior que 20');
+} else {
+    console.log('O valor da média aritmética é menor ou igual a 20');
+};
