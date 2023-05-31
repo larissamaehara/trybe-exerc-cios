3bd713b4bbfbde138f0c62fa2a202242352b6158
Larissa Maehara <larissalali.m@gmail.com>
2023-05-31 15:52:25 -0300

    desafio-fatorial

---
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-02-array-e-estrutura-de-repeticao/exercicios/desafio-fatorial.js
@@ -0,0 +1,10 @@
+//Algoritmo que imprima na tela o fatorial de 10
+
+let fatorial = 10;
+let resultado = fatorial;
+
+for (let index = 1; index <= fatorial; index++) {
+    resultado *= index;
+};
+
+console.log(resultado);
