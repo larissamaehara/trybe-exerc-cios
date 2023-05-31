24a163ec5d4678c90ca191c939ba8339b3a84bf9
Larissa Maehara <larissalali.m@gmail.com>
2023-05-31 15:37:33 -0300

    array e loop - exercicio 5

--- 01-fundamentos/secao-03-introducao-a-javascript/dia-02-array-e-estrutura-de-repeticao/exercicios/array-e-loop5.js
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-02-array-e-estrutura-de-repeticao/exercicios/array-e-loop5.js
@@ -1,3 +1,13 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 //5-Utilizando for, descubra o maior valor contido no array e imprima-o.
+
+let comparar = numbers[0];
+
+for (let index = 1; index < numbers.length; index += 1) {
+    if (numbers[index] > comparar) {
+        comparar = numbers[index];
+    }
+};
+
+console.log(comparar);
