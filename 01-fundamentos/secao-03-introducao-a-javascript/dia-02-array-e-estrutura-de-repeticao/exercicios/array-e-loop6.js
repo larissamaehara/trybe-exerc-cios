c0a9bce5672c12f05f6997400ae5fc705738c896
Larissa Maehara <larissalali.m@gmail.com>
2023-05-31 15:43:44 -0300

    array e loop - exercicio 6

--- 01-fundamentos/secao-03-introducao-a-javascript/dia-02-array-e-estrutura-de-repeticao/exercicios/array-e-loop6.js
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-02-array-e-estrutura-de-repeticao/exercicios/array-e-loop6.js
@@ -2,3 +2,16 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 //6-Descubra quantos valores ímpares existem no array e imprima o resultado. 
 //Caso não exista nenhum, imprima a mensagem: “Nenhum valor ímpar encontrado”.
+let quantoImpares = 0;
+
+for (let index = 0; index < numbers.length; index +=1) {
+    if (numbers[index] % 2 !== 0) {
+        quantoImpares += 1;
+    }
+};
+
+if(quantoImpares===0){
+    console.log("Nenhum valor ímpar encontrado");
+} else {
+    console.log(quantoImpares);
+};
