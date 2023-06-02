bbc3792fd0d30d898034b9532167035d6f5f33f2
Larissa Maehara <larissalali.m@gmail.com>
2023-06-02 11:12:45 -0300

    exercicio1

---
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-04-objetos/exercicios/exercicio1.js
@@ -0,0 +1,16 @@
+const reader = {
+    name: 'Julia',
+    lastName: 'Pessoa',
+    age: 21,
+    favoriteBooks: [
+      {
+        title: 'O Senhor dos Anéis - a Sociedade do Anel',
+        author: 'J. R. R. Tolkien',
+        publisher: 'Martins Fontes',
+      },
+    ],
+  };
+
+  let message = `O livro favorito de ${reader.name} ${reader.lastName} se chama "${reader.favoriteBooks[0].title}".`
+
+  console.log(message);
