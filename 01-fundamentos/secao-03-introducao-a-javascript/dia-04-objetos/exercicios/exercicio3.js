42f7a2dfea2efe4ef0042fbc24a2ce8c8c6eb2a5
Larissa Maehara <larissalali.m@gmail.com>
2023-06-02 12:24:27 -0300

    exercicio3

---
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-04-objetos/exercicios/exercicio3.js
@@ -0,0 +1,36 @@
+const school = {
+    lessons: [
+      {
+        course: 'Python',
+        students: 20,
+        professor: 'Carlos Patrício',
+        shift: 'Manhã',
+      },
+      {
+        course: 'Kotlin',
+        students: 10,
+        professor: 'Gabriel Oliva',
+        shift: 'Noite',
+      },
+      {
+        course: 'JavaScript',
+        students: 738,
+        professor: 'Gustavo Caetano',
+        shift: 'Tarde',
+      },
+      {
+        course: 'MongoDB',
+        students: 50,
+        shift: 'Noite',
+      },
+    ]
+  };
+
+  //item 1: função que obtenha o valor da chave de acordo com sua posição no array.
+
+  //item 2: função que retorne a soma do número total de estudantes em todos os cursos.
+
+  //item 3: função que verifica se uma determinada chave existe em todos os elementos do array lessons. O retorno deve ser um booleano (true ou false). Essa função deve possuir dois parâmetros: o objeto e o nome da chave.
+
+  //item 4: função para alterar o turno para noite no curso de Python. Essa função deve ter três parâmetros: a base de dados a ser modificada, o nome do curso e o novo valor da chave.
+
