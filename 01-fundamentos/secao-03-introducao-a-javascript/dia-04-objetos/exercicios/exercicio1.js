e24926403d1d6e765e7b7446a07337be1a90d3f4
Larissa Maehara <larissalali.m@gmail.com>
2023-06-02 11:55:59 -0300

    exercicio1

--- 01-fundamentos/secao-03-introducao-a-javascript/dia-04-objetos/exercicios/exercicio1.js
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-04-objetos/exercicios/exercicio1.js
@@ -25,3 +25,13 @@ reader.favoriteBooks.push({
 console.log(reader);
 
 //item 3:
+let message2 = `Julia tem ${reader.favoriteBooks.length} livros favoritos.`
+
+console.log(message2);
+
+// Nessa solução, temos duas saídas. Se o número de livros favoritos for maior que 1, escreve "livros favoritos", no plural. Se não for maior que 1, escreve no singular: "livro favorito".
+const numFavoriteBooks = reader.favoriteBooks.length;
+
+numFavoriteBooks > 1 
+  ? console.log(`${reader.name} tem ${numFavoriteBooks} livros favoritos.`) 
+  : console.log(`${reader.name} tem ${numFavoriteBooks} livro favorito.`);
