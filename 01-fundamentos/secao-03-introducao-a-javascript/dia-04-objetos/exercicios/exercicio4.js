adf2e1fba175b6bfa604e32d36093cd9565f9eb0
Larissa Maehara <larissalali.m@gmail.com>
2023-06-02 13:40:50 -0300

    exercicio4

--- 01-fundamentos/secao-03-introducao-a-javascript/dia-04-objetos/exercicios/exercicio4.js
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-04-objetos/exercicios/exercicio4.js
@@ -17,4 +17,35 @@ const basket = [
     'Pera', 'Melancia', 'Jaca', 'Banana', 'Laranja', 'Jaca',
     'Banana', 'Pera', 'Abacate', 'Uva',
   ];
-  
+
+  // Criação de um objeto que contenha o nome da fruta como chave e a quantidade de vezes que ela aparece no array como valor:
+const countingFruit = {};
+
+// Loop que irá contar quantas vezes cada fruta aparece no array basket
+for (let index = 0; index < basket.length; index += 1) {
+  let fruit = basket[index];
+  // Condicional: SE a propriedade com o nome da fruta ainda não existir, então ela será criada com o valor 1. Caso ela já exista, vamos incrementar o valor.
+  if (!countingFruit[fruit]) {
+    countingFruit[fruit] = 1;
+  } else {
+    countingFruit[fruit] += 1;
+  }
+};
+
+// Convertemos o objeto result em um array
+const entries = Object.entries(countingFruit);
+
+// Criação de um novo array
+let newArray = [];
+
+// Loop que irá verificar se o número de frutas é maior ou não do que 1. Caso seja maior, adicionamos a letra "s".
+for (let index = 0; index < entries.length; index += 1) {
+  if (entries[index][1] > 1) {
+    newArray.push(`${entries[index][1]} ${entries[index][0]}s`);
+  } else {
+    newArray.push(`${entries[index][1]} ${entries[index][0]}`);
+  }
+};
+
+// Exibimos a string juntando os valores do array "newArray" com uma vírgula e um espaço em branco.
+console.log(`Sua cesta possui: ${newArray.join(', ')}.`);
