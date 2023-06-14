62b693dd026bb8cfb83054857a9ea9bdbe0bf7d9
Larissa Maehara <larissalali.m@gmail.com>
2023-06-14 15:52:42 -0300

    exercicio-secao-4-dia-3

--- 01-fundamentos/secao-04-javascript-DOM-eventos-e-web-storage/dia-03-javascript-eventos/addEventListener-exercicio-aula/main.js
+++ 01-fundamentos/secao-04-javascript-DOM-eventos-e-web-storage/dia-03-javascript-eventos/addEventListener-exercicio-aula/main.js
@@ -10,20 +10,44 @@ const myWebpage = document.getElementById('my-spotrybefy');
 
 // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando este for clicado.
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como é possível fazer isso? Dica: Lembre-se do método `.classList.remove`.
+const addClassTech = (event) => {
+  const addTechElement = document.querySelector('.tech');
+  addTechElement.classList.remove('tech');
+  event.target.classList.add('tech');
+  input.value = '';
+}
+
+firstLi.addEventListener('click', addClassTech);
+secondLi.addEventListener('click', addClassTech);
+thirdLi.addEventListener('click', addClassTech);
 
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech'.
+input.addEventListener('input', (event) => {
+  const addTechElement = document.querySelector('.tech');
+  addTechElement.innerText = event.target.value;
+});
 
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy',
 // redirecione para alguma página.
 // 4.1. Que tal redirecionar para seu portfólio? Dica: Lembre-se dos métodos `window.location.replace` e `window.open`.
+myWebpage.addEventListener('dblclick', () => {
+  window.location.replace('https://larissamaehara.github.io/');
+});
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo.
+myWebpage.addEventListener('mouseover',(event) => {
+  event.target.style.color = 'green';
+});
+
+myWebpage.addEventListener('mouseout', (event) => {
+  event.target.style.color = 'unset';
+});
 
 // Segue abaixo um exemplo de uso do event.target.
 
-const resetText = (event) => {
+function resetText(event) {
   // O event é passado como um parâmetro para a função.
   event.target.innerText = 'Opção reiniciada';
   // O event possui várias propriedades, porém a mais usada é o event.target,
