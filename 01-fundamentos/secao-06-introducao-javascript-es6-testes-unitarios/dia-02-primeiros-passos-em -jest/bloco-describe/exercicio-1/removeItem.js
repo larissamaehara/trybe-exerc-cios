370e552daa0bcf8dfe48237c384da22bf3445e1c
Larissa Maehara <larissalali.m@gmail.com>
2023-07-02 17:08:38 -0300

    :sparkles: feat: bloco describe - Exercicio 1

---
+++ 01-fundamentos/secao-06-introducao-javascript-es6-testes-unitarios/dia-02-primeiros-passos-em -jest/bloco-describe/exercicio-1/removeItem.js
@@ -0,0 +1,19 @@
+/* A função removeItem(arr, item) recebe um array arr e retorna uma cópia desse array sem o elemento item, caso ele exista no array.
+
+Verifique se a chamada removeItem([1, 2, 3, 4], 3) retorna o array esperado.
+Verifique se a chamada removeItem([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4].
+Verifique se a chamada removeItem([1, 2, 3, 4], 5) retorna o array esperado. */
+
+// removeItem.js
+
+function removeItem (arr, item) {
+  const newArr = []
+  for (let index = 0; index < arr.length; index += 1) {
+    if (item !== arr[index]) {
+      newArr.push(arr[index])
+    }
+  }
+  return newArr
+}
+
+module.exports = removeItem
