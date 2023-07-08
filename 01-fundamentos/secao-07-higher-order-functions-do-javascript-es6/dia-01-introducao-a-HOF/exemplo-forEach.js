ae7ce57f185f0c1883d8244c154cd550a4a959e2
Larissa Maehara <larissalali.m@gmail.com>
2023-07-08 07:06:41 -0300

    :sparkles: feat: exemplos da aula

--- 01-fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-01-introducao-a-HOF/exemplo-forEach.js
+++ 01-fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-01-introducao-a-HOF/exemplo-forEach.js
@@ -4,3 +4,22 @@ pessoasAprovadas.forEach((nome, index) => {
   pessoasAprovadas[index] = pessoasAprovadas[index].toUpperCase() // acessa cada elemento do array e atualiza para letra maiúscula
 })
 console.log(pessoasAprovadas) // ['ANA BEATRIZ', 'CAIO NUNES', 'AFONSO RIBEIRO', 'LEONARDO LINS']
+
+/* const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+numbers.forEach((element) => {
+  console.log(element * 2) // 0, 2, 4, 6, 8, 10, 12, 14, 16, 18
+}) */
+
+// Você também pode deixar esse código em apenas uma linha. Para isso, basta remover as chaves {} e deixar o console logo após a arrow
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+numbers.forEach((element) => console.log(element * 2))
+
+// Você também pode separar a callback do forEach em uma outra função. Veja o exemplo abaixo:
+
+const numbersArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+const multiply = (element) => console.log(element * 2)
+
+numbersArray.forEach(multiply)
