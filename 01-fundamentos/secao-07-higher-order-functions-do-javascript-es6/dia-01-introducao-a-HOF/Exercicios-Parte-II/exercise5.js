734492f4809c1b52fee54bcce82881a703392177
Larissa Maehara <larissalali.m@gmail.com>
2023-07-08 10:27:31 -0300

    :zap: perf: Parte II  Exercicio-5

--- 01-fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-01-introducao-a-HOF/Exercicios-Parte-II/exercise5.js
+++ 01-fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-01-introducao-a-HOF/Exercicios-Parte-II/exercise5.js
@@ -62,3 +62,8 @@ const books = [
 ]
 
 // Faça uma função que retorne true, se algum livro foi lançado na década de 80, e false, caso contrário.
+const someBookWasReleaseOnThe80s = () => {
+  return books.some((book) => book.releaseYear >= 1980 && book.releaseYear <= 1989)
+}
+
+console.log(someBookWasReleaseOnThe80s())
