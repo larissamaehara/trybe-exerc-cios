21c6ebcb1787a80f702418d5d5de2684d2bca293
Larissa Maehara <larissalali.m@gmail.com>
2023-07-08 10:29:17 -0300

    :zap: perf: Parte II  Exercicio-6

--- 01-fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-01-introducao-a-HOF/Exercicios-Parte-II/exercise6.js
+++ 01-fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-01-introducao-a-HOF/Exercicios-Parte-II/exercise6.js
@@ -63,4 +63,9 @@ const books = [
 
 // Faça uma função que retorne true, caso nenhuma pessoa autora tenha nascido no mesmo ano, e false, caso contrário.
 
-
+const authorUnique = () => {
+  return books.every((book) =>
+    !books.some((bookSome) =>
+      (bookSome.author.birthYear === book.author.birthYear) && (bookSome.author.name !== book.author.name)))
+}
+console.log(authorUnique())
