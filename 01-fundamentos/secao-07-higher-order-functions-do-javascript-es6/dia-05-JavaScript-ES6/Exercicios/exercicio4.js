4bb2ea14030d845f6a55d9180eaf8ccb23cd5bb5
Larissa Maehara <larissalali.m@gmail.com>
2023-07-11 16:24:40 -0300

    :sparkles: feat: Exercicios

---
+++ 01-fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-05-JavaScript-ES6/Exercicios/exercicio4.js
@@ -0,0 +1,21 @@
+// Escreva uma função getLastName que receba como parâmetro um objeto contendo informações de uma pessoa.
+// Essa função deve retornar a propriedade lastName para o objeto passado, porém,
+// caso o objeto não tenha essa propriedade, a função deve retornar a mensagem lastName não preenchido.
+const student1 = {
+  name: 'Claudia',
+  lastName: 'Farias',
+  age: 23
+}
+
+const student2 = {
+  name: 'Vitor',
+  age: 20
+}
+
+const getLastName = (objStudent) => {
+  const { lastName = 'lastName não preenchido' } = objStudent
+  return lastName
+}
+
+console.log(getLastName(student1))
+console.log(getLastName(student2))
---
+++ 01-fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-05-JavaScript-ES6/Exercicios/exercicio5.js
@@ -0,0 +1,10 @@
+// Usando array destructuring, armazene cada nome presente na variável moreStudents em variáveis separadas:
+const moreStudents = [
+  'Chris',
+  ['Ahmad', 'Antigoni'],
+  ['Toby', 'Sam']
+]
+
+const [student1, [student2, student3], [student4, student5]] = moreStudents
+
+console.log(student1, student2, student3, student4, student5)
