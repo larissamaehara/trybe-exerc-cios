e7e9732bcace221c553bcbeec61b301196f3fe52
Larissa Maehara <larissalali.m@gmail.com>
2023-09-05 09:15:56 -0300

    :sparkles: feat: secao-04-introducao-RTL

--- 02-front-end/secao-04-testes-automatizados-com-react-testing-library/dia-01-introducao-ao-react-testing-library/configurando-RTL/configurando-rtl/src/App.tsx
+++ 02-front-end/secao-04-testes-automatizados-com-react-testing-library/dia-01-introducao-ao-react-testing-library/configurando-RTL/configurando-rtl/src/App.tsx
@@ -1,14 +1,44 @@
+import { useState } from 'react';
+import ValidEmail from './components/ValidEmail';
+
 import './App.css';
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [title, setTitle] = useState('');
+
+  const handleSendEmail = () => {
+    setTitle(email);
+    setEmail('');
+  };
+
   return (
     <div className="App">
-      <label htmlFor="id-email">
-        Email
-        <input id="id-email" type="email" />
-      </label>
-      <input data-testid="id-send" type="button" value="Enviar" />
-      <input type="button" value="Voltar" />
+      <div className="form-container">
+        <label htmlFor="id-email">
+          Email:
+          <input
+            id="id-email"
+            type="email"
+            onChange={ (e) => setEmail(e.target.value) }
+            value={ email }
+          />
+        </label>
+        <input
+          id="btn-send"
+          type="button"
+          data-testid="id-send"
+          value="Enviar"
+          onClick={ handleSendEmail }
+        />
+        <input
+          id="btn-back"
+          type="button"
+          value="Voltar"
+          onClick={ () => setTitle('') }
+        />
+      </div>
+      <ValidEmail email={ title } />
     </div>
   );
 }
