465ad3fb20294bc33e4fe8803622fe38b41d6400
Larissa Maehara <larissalali.m@gmail.com>
2023-09-06 19:18:32 -0300

    :sparkles: feat: RTL:Mocks - exercicio-de-fixacao

--- 02-front-end/secao-04-testes-automatizados-com-react-testing-library/dia-02-RTL-mocks/rtl-mocks/src/App.test.tsx
+++ 02-front-end/secao-04-testes-automatizados-com-react-testing-library/dia-02-RTL-mocks/rtl-mocks/src/App.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { vi } from 'vitest';
 import App from './App';
 
@@ -35,5 +36,24 @@ describe('Testando fetch', () => {
     expect(renderedJoke).toBeInTheDocument();
     expect(mockFetch).toHaveBeenCalledTimes(1);
     expect(mockFetch).toHaveBeenCalledWith('https://icanhazdadjoke.com/', { headers: { Accept: 'application/json' } });
+
+    const MOCK_JOKE_2 = {
+      id: '8s5bGtrOfxc',
+      joke: 'Whats red and bad for your teeth? A Brick.',
+    };
+
+    const MOCK_RESPONSE_2 = {
+      ok: true,
+      status: 200,
+      json: async () => MOCK_JOKE_2,
+    } as Response;
+
+    const secondMockFetch = vi.spyOn(global, 'fetch').mockResolvedValueOnce(MOCK_RESPONSE_2);
+    const newJokeButton = screen.getByRole('button', { name: 'New joke' });
+    await userEvent.click(newJokeButton);
+
+    const secondRenderedJoke = await screen.findByText('Whats red and bad for your teeth? A Brick.');
+    expect(secondRenderedJoke).toBeInTheDocument();
+    expect(secondMockFetch).toHaveBeenCalledTimes(1);
   });
 });
--- 02-front-end/secao-04-testes-automatizados-com-react-testing-library/dia-02-RTL-mocks/rtl-mocks/src/App.tsx
+++ 02-front-end/secao-04-testes-automatizados-com-react-testing-library/dia-02-RTL-mocks/rtl-mocks/src/App.tsx
@@ -2,6 +2,9 @@ import { useState, useEffect } from 'react';
 import fetchJoke from './utils/fetchJoke';
 import './App.css';
 
+// Criando um botão chamado “New Joke” que, ao ser clicado,
+// faz uma nova requisição para API para atualizar a piada que está sendo renderizada na tela.
+
 function App() {
   const [joke, setJoke] = useState('');
 
