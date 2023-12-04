1efa3252635a42adb9dbe6715f405efcf1e69690
Larissa Maehara <larissalali.m@gmail.com>
2023-12-04 18:37:01 -0300

    :sparkles: feat: 3.5 - Aprendendo Express e MySQL

---
+++ 03-back-end/secao-03-introducao-ao-desenvolvimento-web-com-node.js/dia-05-node.js-express-e-MySQL/aprendendo-express-MySQL/trybecash-api/src/db/connection.js
@@ -0,0 +1,16 @@
+// Precisaremos criar em nossa aplicação o arquivo src/db/connection.js,
+// que será responsável por realizar a conexão com o servidor MySQL utilizando a biblioteca mysql2
+const mysql = require('mysql2/promise');
+
+const connection = mysql.createPool({
+  host: process.env.MYSQL_HOST || 'localhost',
+  port: process.env.MYSQL_PORT || 33060,
+  user: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || 'root',
+  database: process.env.DB_NAME || 'trybecashdb',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+module.exports = connection;
