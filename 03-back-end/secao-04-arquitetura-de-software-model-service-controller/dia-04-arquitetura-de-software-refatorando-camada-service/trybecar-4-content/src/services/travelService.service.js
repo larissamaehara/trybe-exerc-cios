dece0b071cd175c5a3ab499bd50c7c1f5cd55df4
Larissa Maehara <larissalali.m@gmail.com>
2024-01-15 15:38:31 -0300

    :sparkles: feat: Refatorando 4.4

--- 03-back-end/secao-04-arquitetura-de-software-model-service-controller/dia-04-arquitetura-de-software-refatorando-camada-service/trybecar-4-content/src/services/travelService.service.js
+++ 03-back-end/secao-04-arquitetura-de-software-model-service-controller/dia-04-arquitetura-de-software-refatorando-camada-service/trybecar-4-content/src/services/travelService.service.js
@@ -2,12 +2,15 @@ const schema = require('./validations/validationsInputValues');
 const { passengerModel, travelModel } = require('../models');
 
 const requestTravel = async (travelDataObject) => {
+  /* (1) ✅ Validar dados de entrada com Joi; */
   const error = schema.validateRequestTravel(travelDataObject);
   if (error) return { status: error.status, data: { message: error.message } };
 
+  /* (2) ✅ Validar se pessoa passageira existe; */
   const passenger = await passengerModel.findById(travelDataObject.passengerId);
   if (!passenger) return { status: 'NOT_FOUND', data: { message: 'Passenger not found' } };
 
+   /* (3) ✅ Gravar nova solicitação de viagem no banco */
   const newTravelId = await travelModel.insert(travelDataObject);
   const newTravel = await travelModel.findById(newTravelId);
   return { status: 'SUCCESSFUL', data: newTravel };
