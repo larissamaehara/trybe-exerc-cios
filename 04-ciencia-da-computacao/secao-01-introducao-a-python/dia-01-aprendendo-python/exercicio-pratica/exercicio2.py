cbcb5266d1baaf472d94efdbfcc3ef219a5a9ab1
Larissa Maehara <larissalali.m@gmail.com>
2024-03-25 15:34:35 -0300

    :sparkles: feat: Introduçao python

---
+++ 04-ciencia-da-computacao/secao-01-introducao-a-python/dia-01-aprendendo-python/exercicio-pratica/exercicio2.py
@@ -0,0 +1,7 @@
+# 🚀 Exercício 2: Calcule a média aritmética dos valores contidos em uma lista.
+
+def mean(numbers):
+    total = 0
+    for number in numbers:
+        total += number
+    return total / len(numbers)
