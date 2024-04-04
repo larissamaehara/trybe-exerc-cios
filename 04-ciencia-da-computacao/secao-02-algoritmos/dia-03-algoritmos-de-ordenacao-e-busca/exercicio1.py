51d0895d00fc0abd7840624300bc1c01c54231a7
Larissa Maehara <larissalali.m@gmail.com>
2024-04-04 20:03:29 -0300

    :sparkles: feat: Exercicio na pratica

---
+++ 04-ciencia-da-computacao/secao-02-algoritmos/dia-03-algoritmos-de-ordenacao-e-busca/exercicio1.py
@@ -0,0 +1,8 @@
+# Exercício 1: 
+# Dado um array com os seguintes elementos ["zebra", "macaco", "elefante", "arara", "javali"],
+# após duas iterações utilizando bubble sort, como estaria este array?
+
+# ordem alfabética, na primeira iteração "zebra" é movimentada até o final do array
+# na segunda iteração "macaco" é movimentado até a segunda posição do array
+
+# temos então: ["elefante", "arara", "javali", "macaco", "zebra"]
