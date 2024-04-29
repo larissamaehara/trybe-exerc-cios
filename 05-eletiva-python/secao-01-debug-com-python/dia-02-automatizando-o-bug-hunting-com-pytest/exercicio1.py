357b80a0df5051d9a6d3bcd1d6e226899d198e8e
Larissa Maehara <larissalali.m@gmail.com>
2024-04-29 17:59:22 -0300

    :sparkles: feat: Exercicio para fixar - doctests

---
+++ 05-eletiva-python/secao-01-debug-com-python/dia-02-automatizando-o-bug-hunting-com-pytest/exercicio1.py
@@ -0,0 +1,11 @@
+def sum_two_numbers(a, b):
+    """Retorna a soma de dois números recebidos por parâmetro.
+
+    Exemplos
+    --------
+    >>> sum_two_numbers(0, 0)
+    0
+    >>> sum_two_numbers(2, 2) # Novo caso de teste
+    4
+    """
+    return a + b # Bug corrigido
