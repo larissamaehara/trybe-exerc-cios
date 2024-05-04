59fabb4cf9680bae633fcc3752c6d6bd26a5d02e
Larissa Maehara <larissalali.m@gmail.com>
2024-05-04 18:52:26 -0300

    :sparkles: feat: Exercicios

---
+++ 05-eletiva-python/secao-02-POO/dia-02-tipagem-e-pilares-da-POO/exercicio1.py
@@ -0,0 +1,5 @@
+# Exercício 1
+# Modifique o código abaixo para que ele seja tipado corretamente.
+
+def add_two_numbers(num1: int, num2: int) -> int:
+    return num1 + num2
---
+++ 05-eletiva-python/secao-02-POO/dia-02-tipagem-e-pilares-da-POO/exercicio2.py
@@ -0,0 +1,9 @@
+# Exercício 2
+# Modifique o código abaixo para que ele seja tipado corretamente.
+
+class Person:
+    def __init__(self, name: str, age: int, height: float) -> None:
+        self.name = name
+        self.age = age
+        self.height = height
+        
