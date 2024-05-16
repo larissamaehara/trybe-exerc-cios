4265680204c9fdf9bf516f7a01b01455cbbdebe7
Larissa Maehara <larissalali.m@gmail.com>
2024-05-16 16:39:27 -0300

    :sparkles: feat: Para Fixar

---
+++ 05-eletiva-python/secao-03-Flask/dia-04-server-side-rendering-em-flask/para-fixar/src/run_seeds.py
@@ -0,0 +1,12 @@
+from models.joke_model import JokeModel
+
+jokes = [
+    "Porque a galinha atravessou a rua? Para chegar do outro lado",
+    "Qual o contrário de volátil? Vem cá sobrinho",
+    "O que é um pontinho amarelo no céu? Um Yellowcóptero",
+    "O que é um pontinho verde no canto da sala? Uma ervilha fazendo exercício",
+    "O que é um pontinho azul no canto da sala? Um smurfs fazendo exercício"
+]
+
+for joke in jokes:
+    JokeModel({'joke': joke}).save()
