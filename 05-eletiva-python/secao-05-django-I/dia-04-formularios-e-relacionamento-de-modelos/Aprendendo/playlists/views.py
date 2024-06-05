d0da4043caa05e604459e9f4ee209139cc4b25a2
Larissa Maehara <larissalali.m@gmail.com>
2024-06-05 20:37:26 -0300

    :sparkles: feat: Formulários e Relacionamentos - Django

---
+++ 05-eletiva-python/secao-05-django-I/dia-04-formularios-e-relacionamento-de-modelos/Aprendendo/playlists/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render, redirect
+from playlists.forms import CreateMusicForm, CreateMusicModelForm, CreateSingerForm
+from playlists.models import Music, Singer
+
+# Create your views here.
+
+def index(request):
+    context = {"musics": Music.objects.all()}
+    return render(request, "home.html", context)
+
+
+def music(request):
+    form = CreateMusicForm()
+
+    if request.method == "POST":
+        form = CreateMusicForm(request.POST)
+
+        if form.is_valid():
+            Music.objects.create(**form.cleaned_data)
+            return redirect("home-page")
+        
+    context = {"form": form}
+    
+    return render(request, "music.html", context)
+
+def singer(request):
+    form = CreateSingerForm()
+
+    if request.method == "POST":
+        form = CreateSingerForm(request.POST)
+
+        if form.is_valid():
+            Singer.objects.create(**form.cleaned_data)
+            return redirect("home-page")
+
+    context = {"form": form}
+
+    return render(request, "singer.html", context)
+
+def create_music(request):
+    # form = CreateMusicForm()
+    form = CreateMusicModelForm()
+
+    if request.method == "POST":
+        # form = CreateMusicForm(request.POST)
+        form = CreateMusicModelForm(request.POST)
+
+        if form.is_valid():
+            Music.objects.create(**form.cleaned_data)
+            return redirect("home-page")
+
+    context = {"form": form}
+
+    return render(request, "index.html", context)
