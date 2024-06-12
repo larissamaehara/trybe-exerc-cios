5bb0ed45fa5308a96d477766ec7285f98db781e4
Larissa Maehara <larissalali.m@gmail.com>
2024-06-12 18:53:10 -0300

    :sparkles: feat: Aprendendo

---
+++ 05-eletiva-python/secao-06-django-II/dia-01-autenticação/vamos-praticar/personal_trainers_app/trainers/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from rest_framework import routers
+from trainers.views import ClientViewSet
+
+router = routers.DefaultRouter()
+router.register(r'clients', ClientViewSet)
+
+urlpatterns = [
+    path('api/', include(router.urls)),
+]
